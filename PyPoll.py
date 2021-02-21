df1bdc37e4dcb1aff7dfb6e1b37f22aa5d796f86
dasavage1412 <77762219+dasavage1412@users.noreply.github.com>
2021-02-20 19:48:00 -0800

    Add files via upload

--- PyPoll.py
+++ PyPoll.py
@@ -7,14 +7,48 @@
 import  csv
 import os
 file_to_load = 'election_results.csv'
+# Set total votes to 0
+total_votes = 0
+candidate_options = []
+candidate_votes = {}
+winning_candidate = ""
+winning_count = 0
+winning_percentage = 0
+
 with open(file_to_load) as election_data:
 # To Do read and analyze here
     file_reader = csv.reader(election_data) 
     headers = next(file_reader)
-    print(headers)
+    
+    for row in file_reader:
+        total_votes += 1
+        
+        candidate_name = row [2]
 
+        if candidate_name not in candidate_options:
+             candidate_options.append(candidate_name)
+             candidate_votes[candidate_name] = 0  
+        candidate_votes[candidate_name] += 1
+   
+    for candidate_name in candidate_votes:
 
-
+        votes = candidate_votes[candidate_name]
+        vote_percentage = float(votes) / float(total_votes) * 100
+        #format vote_percentage to one decimal  
+       
+        if (votes > winning_count) and (vote_percentage > winning_percentage):
+           winning_count = votes
+           winning_percentage = vote_percentage
+           winning_candidate = candidate_name  
+        print(f"{candidate_name}: {vote_percentage:.1f}% ({votes:,})\n")
+    winning_candidate_summary = (
+        f"---------------------------------\n"
+        f"Winner: {winning_candidate}\n"
+        f"Winning Vote Count: {winning_count:,}\n"
+        f"Winning Percentage: {winning_percentage:.1f}%\n"
+        f"---------------------------------\n"
+    )
+print(winning_candidate_summary)
 
 
 
